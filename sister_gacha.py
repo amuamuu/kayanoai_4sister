82da35a28c51c6b301b9fd8ccf29fb2fe83070c7
amuamuu <stayfreeyour.misery.d.o.d@gmail.com>
2017-09-23 10:29:32 +0900

    ループを実装

--- sister_gacha.py
+++ sister_gacha.py
@@ -3,11 +3,13 @@ import random
 # おねえちゃん
 sister = ['遠山りん', '佐倉慈', '棗ニナ', '保登モカ', 'ライネ']
 
-
 # レアリティ
 rare = ['[N]', '[R]', '[SSR]', '[UR]']
 
 # n連ガチャ
-n = input('何回引く？:')
-for i in range(int(n)):
-    print(random.choice(rare) + random.choice(sister))
+while True:
+    n = input('何回引く？ [qキーで終了]:')
+    if n == 'q':    # 終了
+        break
+    for i in range(int(n)):
+        print(random.choice(rare) + random.choice(sister))
